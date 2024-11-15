ccb76712594e725acf713f1bd091b30270a38d44
Stuart Forrester <stforrester@gmail.com>
2024-11-15 05:31:52 -0600

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,28 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //declares new array that will be the result of using .map on tutorials with a callback function to perform the transformation
+  let titleCasedTutorials = tutorials.map((title) => {
+    //splits each title in the tutorials array into an array of words
+    let wordArray = title.split(" ");
+    
+    //returns an array of words with the first letter capitalized
+    //declares a new array that will be the result of using .map on wordArray with a callback function
+    let capitalizedWordArray = wordArray.map((word) => {
+      //grabs the first letter of each word in wordArray
+      let firstLetter = word.at(0);
+      //grabs the remaining letters of each word in wordArray
+      let remainder = word.slice(1);
+      //capitalizes the first letter we grabbed with the firstLetter variable
+      let firstLetterCapitalized = firstLetter.toUpperCase();
+      //joins the capitalized first letter with the remaining letters in the word
+      let capitalizedWord = firstLetterCapitalized.concat(remainder);
+      //returns the newly capitalized word and adds it to the new array "capitalizedWordArray"
+      return capitalizedWord;
+    })
+    //returns the "capitalizedWordArray" that has been turned into a single title string with each word element separated by a space
+    return capitalizedWordArray.join(" ");
+  })
+  //returns the updated Array of tutorial titles that are now title-cased
+  return titleCasedTutorials;
 }
